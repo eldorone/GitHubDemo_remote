963100bf2cf3768dbf797017dcfb352b6c56b0ab
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-09-11 18:10:49 +0900

    Initial Commit

---
+++ GitHubDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubDemo
+//
+//  Created by Eldor Alikuvatov on 2022/09/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Eldor Alikuvatov on 2022/09/11.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
